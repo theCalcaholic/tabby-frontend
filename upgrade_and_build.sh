a8ac405646fdeb5856c477639ce02703217a5ca3
Tobias Knöppler <3knoeppl@informatik.uni-hamburg.de>
2017-10-22 21:15:15 +0200

    Make update script more verbose.

--- upgrade_and_build.sh
+++ upgrade_and_build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 cd "${0%/*}"
-sudo su www-data -s /bin/sh -c "git pull"
-sudo su www-data -s /bin/sh -c "npm install"
-sudo su www-data -s /bin/sh -c "ng build --prod"
+echo updating source... && \
+sudo su www-data -s /bin/sh -c "git pull" && \
+echo done.
+echo installing node modules... && \
+sudo su www-data -s /bin/sh -c "npm install" && \
+echo done.
+echo building source... && \
+sudo su www-data -s /bin/sh -c "ng build --prod" && \
+echo done.
